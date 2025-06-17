1ba4fb6655e728511ef4d931418da9fc0b22bb8f
Venkata Subbarao Gorantla <gvsrusa@gmail.com>
2025-06-17 21:50:17 +0000

    auth

--- backend/user-service/src/main/java/com/miowsis/user/controller/AuthController.java
+++ backend/user-service/src/main/java/com/miowsis/user/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication", description = "Authentication endpoints")
 public class AuthController {
